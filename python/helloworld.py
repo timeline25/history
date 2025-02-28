0e87e43458394e3ca0a0d7732fe5c85d89cb58e4
timeline25 <methodenhcmc@gmail.com>
2025-02-28 20:36:21 +0000

    updated with input feature from codespace

--- python/helloworld.py
+++ python/helloworld.py
@@ -1,4 +1,6 @@
 import math
 
-name = "user"
-print(f"Hello {name}. Welcome to python!")
+username = "unknown.user"
+
+username = input("Please enter your name: ")
+print(f"Hello {username}. Welcome to python!")
